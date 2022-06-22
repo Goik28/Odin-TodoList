32632a440b53fb686d34cce2fbf929a1b1740380
Goik <sbabinski@outlook.com>
2022-06-22 20:39:19 -0300

    Add exclude list button
    Add exclude list button to the list DOM.
    Change the color theme of the lists and the starting position.

--- src/Components/List/listDOM.js
+++ src/Components/List/listDOM.js
@@ -4,21 +4,36 @@ export default createDOMList;
 function createDOMList() {
     const list = document.createElement("div");
     list.className = "list-body";
-    const title = document.createElement("input");
-    title.className = "list-title";
-    title.value = "New List";
+
+
     const taskList = document.createElement("div");
     taskList.className = "list-tasks";
     const addTaskButton = document.createElement("button");
     addTaskButton.className = "list-addButton";
     addTaskButton.textContent = "+";
 
-    list.appendChild(title);
+    list.appendChild(createListHeader());
     list.appendChild(taskList);
     taskList.appendChild(addTaskButton);
     list.appendChild(createListFooter());
 
     return list;
+}
+
+function createListHeader() {
+    const listHeader = document.createElement("div");
+    listHeader.className = "list-header";
+    const title = document.createElement("input");
+    title.className = "list-title";
+    title.value = "New List";
+    const delListButton = document.createElement("button");
+    delListButton.className = "list-delButton";
+    delListButton.textContent = "X";
+
+    listHeader.appendChild(title);
+    listHeader.appendChild(delListButton)
+
+    return listHeader;
 }
 
 function createListFooter() {
